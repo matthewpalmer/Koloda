e26536ee1fced42de159bc7c7e0934f9042a4c3e
matthewpalmer <matt@matthewpalmer.net>
2015-07-19 12:08:16 +1000

    Fix bug introduced when trying to allow optional overlays

--- Pod/Classes/KolodaView/KolodaView.swift
+++ Pod/Classes/KolodaView/KolodaView.swift
@@ -110,9 +110,9 @@ public class KolodaView: UIView, DraggableCardDelegate {
                     nextCardView.alpha = index == 0 ? alphaValueOpaque : alphaValueSemiTransparent
                     nextCardView.userInteractionEnabled = index == 0
                     
-                    if let overlayView = overlayViewForCardAtIndex(UInt(index)) {
-                        nextCardView.configure(nextCardContentView, overlayView: overlayView)
-                    }
+//                    if let overlayView = overlayViewForCardAtIndex(UInt(index)) {
+                    nextCardView.configure(nextCardContentView, overlayView: overlayView)
+//                    }
                     
                     visibleCards.append(nextCardView)
                     index == 0 ? addSubview(nextCardView) : insertSubview(nextCardView, belowSubview: visibleCards[index - 1])
